f82d0778235082ab9b9155eebc7e1d2d363e816d
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-15 13:25:12 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos calcular o montante final de um investimento a juros simples, para isso, digite:");
+
+            Console.WriteLine("Capital [c] (R$):");
+            double capital = Convert.ToDouble(Console.ReadLine()); 
+
+            Console.WriteLine("Taxa de juros [i] (%):");
+            double taxa = Convert.ToDouble(Console.ReadLine()); 
+
+            Console.WriteLine("Tempo [t] (meses):");
+            double tempo = Convert.ToDouble(Console.ReadLine()); 
+
+            double juros = capital * (taxa/100) * tempo; 
+            Console.WriteLine($"Juros (R$): {juros}");
+
+            double montante = capital + juros;
+            Console.WriteLine($"Montante (R$): {montante}");
+
+        }
+    }
+}
